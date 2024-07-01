9f8825266729ff541783be02b839cef2998f5b05
23stas23 <126966210+23stas23@users.noreply.github.com>
2024-07-01 14:17:53 +0100

    Update Version 0.002.py

--- Version 0.002.py
+++ Version 0.002.py
@@ -1,1 +1,23 @@
+import pygame
+from random import randint
+import sys
 
+# Инициализация Pygame
+pygame.init()
+
+# Настройка окна
+WIDTH = 1500
+HEIGHT = 1000
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Farm")
+clock = pygame.time.Clock()
+
+# Цвета
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (25, 25, 255)
+GRAY = (127,140,140)
+GREEN = (0, 255, 17)
+FPS = 60
+font = pygame.font.SysFont("Arial", 32)
+font1 =pygame.font.SysFont("Arial", 22)
