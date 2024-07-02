19febbb5f3815219ac6c2dec99b4cb1e155e7c56
23stas23 <126966210+23stas23@users.noreply.github.com>
2024-07-02 15:09:08 +0100

    Update Version 0.002.py

--- Version 0.002.py
+++ Version 0.002.py
@@ -30,8 +30,7 @@ def bacgraund():
     screen.blit(bacgraund_image, bacgraund_rect)
 
 #items
-items = {"apple": {"id": 1, "name":"Apple", "image":"Image/Appal.png", "cost": 50, "count": 0}, "wood":{"id": 2, "name": "Wood", "image": "Image/Wood.png", "cost": 100, "count": 0}}
-tools = {"axe": {"id": 1, "name": "Axe", "image": "Image/Axe.png", "cost": 50}}
+items = {"apple": {"id": 1, "name":"Apple", "image":"Image/Appal.png", "cost": 50, "count": 0}, "wood":{"id": 2, "name": "Wood", "image": "Image/Wood.png", "cost": 100, "count": 0}, "axe": {"id": 1, "name": "Axe", "image": "Image/Axe.png", "cost": 50}}
 
 #Player
 up_move = False
@@ -62,7 +61,7 @@ player = Player()
 
 #UI Inventory
 InventoryActive = False
-Invntory_slots = pygame.sprite.Group()
+Inventory_slots = pygame.sprite.Group()
 
 UI_Inventory_image = pygame.transform.scale(pygame.image.load("Image/UI_Inventory.png"), (750, 500))
 UI_Inventory_rect = UI_Inventory_image.get_rect()
@@ -72,10 +71,12 @@ UI_Inventory_rect.y = 100
 #Inventory slot
 class UI_Inventory_slot(pygame.sprite.Sprite):
     def __init__(self, x, y, item, actives, font):
+        super().__init__()
+        
         self.active = actives
         self.font = font
+        self.item = item
 
-        #UI_inventory_slot
         self.slot_image = pygame.transform.scale(pygame.image.load("Image/UI_slot_inventory.png"), (80, 80))
         self.slot_rect = self.slot_image.get_rect()
         self.slot_rect.x = UI_Inventory_rect.x + x
@@ -86,17 +87,18 @@ class UI_Inventory_slot(pygame.sprite.Sprite):
         self.item_rect.x = self.slot_rect.x + 5
         self.item_rect.y = self.slot_rect.y + 5
 
-        self.name_item = self.font.render(str(items[item]["name"]), True, WHITE)
-
-        self.count_item = self.font.render(str(items[item]["count"]), True, WHITE)
+        self.name_item = self.font.render(str(items[self.item]["name"]), True, WHITE)
 
     def update(self):
         if InventoryActive:
             screen.blit(self.slot_image, self.slot_rect)
             screen.blit(self.item_image, self.item_rect)
             screen.blit(self.name_item, (self.slot_rect.x + 15, self.slot_rect.y + 75))
+            
+            self.count_item = self.font.render(str(items[self.item]["count"]), True, WHITE)
             screen.blit(self.count_item, (self.slot_rect.x + 65, self.slot_rect.y + 50))
-#Count Inventory slot
+            
+#number slots
 slot = UI_Inventory_slot(10, 10, "apple", InventoryActive, font1)
 slot1 = UI_Inventory_slot(100, 10, "wood", InventoryActive, font1)
 
@@ -104,41 +106,66 @@ Inventory_slots.add(slot, slot1)
 
 #Market
 Market_active = False
+Market_slots =  pygame.sprite.Group()
 
+#UI Market 
+UI_Market_image = pygame.transform.scale(pygame.image.load("Image/UI_inventory.png"), (750, 500))
+UI_Market_rect = UI_Market_image.get_rect()
+UI_Market_rect.x = 100
+UI_Market_rect.y = 100
 
 class UI_Market_slot(pygame.sprite.Sprite):
     def __init__(self, x, y, item, active, font):
-        #UI Market slot
-        UI_Market_slot_image = pygame.transform.scale(pygame.image.load("Image/UI_slot_inventory.png"), (70,70))
-        UI_Market_slot_rect = UI_Market_slot_image.get_rect()
-        UI_Market_slot_rect.x = UI_Market_rect.x + 20
-        UI_Market_slot_rect.y = UI_Market_rect.y + 20
+        super().__init__()
+        self.active = active
+        self.font = font
+        self.item = item
+        
+        self.slot_image = pygame.transform.scale(pygame.image.load("Image/UI_slot_inventory.png"), (70,70))
+        self.slot_rect = self.slot_image.get_rect()
+        self.slot_rect.x = UI_Market_rect.x + x
+        self.slot_rect.y = UI_Market_rect.y + y
 
-        #UI Market slot item
-        UI_Market_slot_item_image = pygame.transform.scale(pygame.image.load("Image/Axe.png"), (50,50))
-        UI_Market_slot_item_rect = UI_Market_slot_item_image.get_rect()
-        UI_Market_slot_item_rect.x = UI_Market_slot_rect.x + 5
-        UI_Market_slot_item_rect.y = UI_Market_slot_rect.y + 5
+        self.item_image = pygame.transform.scale(pygame.image.load("Image/Axe.png"), (50,50))
+        self.item_rect = self.item_image.get_rect()
+        self.item_rect.x = self.slot_rect.x + 5
+        self.item_rect.y = self.slot_rect.y + 5
 
-        #UI Market button
-        UI_Market_button_image = pygame.transform.scale(pygame.image.load("Image/UI_button_Market.png"), (70, 20))
-        UI_Market_button_rect = UI_Market_button_image.get_rect()
-        UI_Market_button_rect.x = UI_Market_slot_rect.x
-        UI_Market_button_rect.y = UI_Market_slot_rect.y + 75
+        self.button_image = pygame.transform.scale(pygame.image.load("Image/UI_button_Market.png"), (70, 20))
+        self.button_rect = self.button_image.get_rect()
+        self.button_rect.x = self.slot_rect.x
+        self.button_rect.y = self.slot_rect.y + 75
 
-        #UI Market text cost Axe
-        cost_Axe = str(tools["axe"]["cost"])
-        cost_item = font1.render(cost_Axe, True, WHITE)
+        self.cost_item_text = font1.render(str(items[self.item]["cost"]), True, WHITE)
+    def update():
+        if Market_active:
+            screen.blit(self.slot_image, self.slot.rect)
+            screen.blit(self.item_image, self.item_rect)
+            screen.blit(self.button_image, self.button_rect)
+            screen.blit(self.cost_item_text, (self.slot_rect2.x + 55, self.slot_rect.y + 50))
+
+#number slots
+market_slot = UI_Market_slot(20, 20, "axe", Market_active, font1)
+market_slot1 = UI_Market_slot(120, 20, "apple", Market_active, font1)
+
+Market_slots.add(market_slot, market_slot1)
+
+        
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_e and InventoryActive == False:
+            if event.key == pygame.K_e and InventoryActive == False and Market_active == False:
                 InventoryActive = True
             else:
                 InventoryActive = False
+            if event.key == pygame.K_m and InventoryActive == False and Market_active == False:
+                Market_active = True
+            else:
+                Market_active = False
+
     screen.fill(BLACK)
     bacgraund()
     #Player render
@@ -147,8 +174,13 @@ while running:
     #UI Inventory render
     if InventoryActive:
         screen.blit(UI_Inventory_image, UI_Inventory_rect)
-    slot.update()
-    slot1.update()
+    Inventory_slots.update()
+
+    #UI Market render
+    if Market_active:
+        screen.blit(UI_Market_image, UI_Market_rect)
+    market_slot.update()
+    
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
