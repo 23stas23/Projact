26ba9481b7279f8908c9838b9791993f1e14b553
23stas23 <126966210+23stas23@users.noreply.github.com>
2024-11-06 10:14:42 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,54 @@
-import sqlite3
+import pygame
+import sys
+import random
+import subprocess
 
-# Подключение к базе данных
-conn = sqlite3.connect('Inventory.db')
-cursor = conn.cursor()
 
-# Создаем таблицу
-cursor.execute('''CREATE TABLE IF NOT EXISTS Inventory (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, counts INTEGER NOT NULL)''')
+# _______initiate Game______________ #
+class Game:
+    pygame.init()
+    width = 800
+    height = 800
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Maze Game")
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
-# Функция для получения текущего значения counts по имени
-def get_count(name):
-    cursor.execute("SELECT counts FROM Inventory WHERE name = ?", (name,))
-    result = cursor.fetchone()
-    return result[0] if result else None
-
-# Функция для обновления значения counts по имени
-def update_count(name, new_count):
-    cursor.execute("SELECT id FROM Inventory WHERE name = ?", (name,))
-    result = cursor.fetchone()
-    if result:
-        cursor.execute("UPDATE Inventory SET counts = ? WHERE name = ?", (new_count, name))
+    white = [255, 255, 255]
+    black = [0, 0, 0]
+    lblue = [159, 210, 255]
+    background = input(
+        "What color background would you like? (White, Black, or Light Blue): ")
+    if background == "White":
+        screen.fill(white)
+        pygame.display.flip()
+    elif background == "Black":
+        screen.fill(black)
+        pygame.display.update()
+    elif background == "Light Blue":
+        screen.fill(lblue)
+        pygame.display.update()
     else:
-        cursor.execute("INSERT INTO Inventory (name, counts) VALUES (?, ?)", (name, new_count))
-    conn.commit()
+        screen.fill(black)
+        pygame.display.update()
+    for "." in "map1.txt":
+        pygame.image.load("winter.Wall.png")
 
-# Обновляем значение counts для Wood
-#update_count('Wood', 10)
 
-# Получаем обновленное значение для Wood
-#updated_count_wood = get_count('Wood')
-#print(f"Updated counts for Wood: {updated_count_wood}")
 
-# Получаем данные из таблицы
-cursor.execute("SELECT * FROM Inventory")
-rows = cursor.fetchall()
+# ___________________TO RUN___________________________ #
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    if event.type == pygame.KEYDOWN:
+        command = "python JakeGame.py"
+        subprocess.call(command)
 
-# Выводим данные
-for row in rows:
-    print(row)
+for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+        if event.type == pygame.K_ESCAPE:
+            pygame.quit()
+# _______________________________________________ #
 
-# Закрываем соединение с базой данных
-conn.close()
+pygame.quit()
