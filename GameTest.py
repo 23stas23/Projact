cecbb4d8d330c7f313c3b8d185d3352ce2dc0100
23stas23 <126966210+23stas23@users.noreply.github.com>
2024-11-06 10:24:16 +0000

    Update GameTest.py

--- GameTest.py
+++ GameTest.py
@@ -1,4 +1,23 @@
 import pygame
 
 class Game:
-  background = pygame.transfor
+  # Инициализация Pygame
+  pygame.init()
+
+  # Настройка окна
+  WIDTH = 1000
+  HEIGHT = 500
+  FPS = 60
+
+  # Цвета
+  BLACK = (0, 0, 0)
+  
+  screen = pygame.display.set_mode((WIDTH, HEIGHT))
+  
+  pygame.display.set_caption("Farm")
+  
+  clock = pygame.time.Clock()
+
+  # Цвета
+  BLACK = (0, 0, 0)
+
