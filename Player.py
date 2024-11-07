80e35056a970f5c9644d3cc30ccc8edb6a785a5e
23stas23 <126966210+23stas23@users.noreply.github.com>
2024-11-07 12:26:41 +0000

    Update Player.py

--- Player.py
+++ Player.py
@@ -2,31 +2,34 @@ import pygame
 from random import randint
 import sys
 
-# Инициализация Pygame
-pygame.init()
-
-# Настройка окна
-WIDTH = 1000
-HEIGHT = 500
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Farm")
-clock = pygame.time.Clock()
-
-# Цвета
-BLACK = (0, 0, 0)
-
-FPS = 60
+class Game:
+    pygame.init()
+    
+    WIDTH = 1000
+    HEIGHT = 500
+    FPS = 60
+    
+    BLACK = (0, 0, 0)
+    
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Farm")
+    clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
 fence_group = []
 hand = True
 hand_open = True
 
+font = pygame.font.SysFont("Arial", 32)
+font1 =pygame.font.SysFont("Arial", 22)
+
+items = {"Apple": {"id": 1, "name":"Apple", "image":"Image/Appal.png", "cost": 50}, "Wood":{"id": 2, "name": "Wood", "image": "Image/Wood.png", "cost": 100}, "Axe": {"id": 1, "name": "Axe", "image": "Image/Axe.png", "cost": 150}}
+
 
 tools_group_equp =["Hand", "Axe"]
-tools = {"Hand":{"image":BLACK},"Axe":{"image": (100, 100, 100)}}
+tools = {"Hand":{"image":"Image/Appel.png"},"Axe":{"image": "Image/Axe.png"}}
 
-
+#________________Player__________________#
 class Player(pygame.sprite.Sprite):
     def __init__(self, speed):
         super().__init__()
@@ -70,15 +73,65 @@ class Player(pygame.sprite.Sprite):
         global tools_group_equp
         if self.tool > 1:
             self.tool = 0
-        name = tools_group_equp[self.tool]
-
+        name = ""
+        name = tools_group_equp[int(self.tool)]
+        image_tool = pygame.transform.scale(pygame.image.load(tools[name]["image"]), (20, 20))
+        rect_tool = image_tool.get_rect()
+        rect_tool.x = player.rect.x + 10
+        rect_tool.y = player.rect.y + 10
+        screen.blit(image_tool, rect_tool)
         
-        
-            
-            
-
 player = Player(3)
 all_sprites.add(player)
+
+#_____________________Inventory__________________________
+class Inventory(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.Active = False
+        self.image = pygame.transform.scale(pygame.image.load("Image/UI_Inventory.png"), (700, 400))
+        self.rect = self.image.get_rect()
+        self.rect.x = 70
+        self.rect.y = 70
+    def update(self):
+        if self.Active:
+            screen.blit(self.image, self.rect)
+        else:
+            pass
+        
+            
+        
+        
+inventory = Inventory()
+#________________________________________Slot_Inventory_________________________________________
+class Slot(pygame.sprite.Sprite):
+    def __init__(self, direction_x, direction_y, name, count):
+        super().__init__()
+        self.x = direction_x
+        self.y = direction_y
+        self.h = 55
+        self.w = 55
+        self.name = name
+        self.count = count
+
+        self.image = pygame.transform.scale(pygame.image.load("Image/UI_slot_inventory.png"), (self.h, self.w))
+        self.rect = self.image.get_rect()
+        self.rect.x = inventory.rect.x + self.x
+        self.rect.y = inventory.rect.y + self.y
+
+        self.item_image = pygame.transform.scale(pygame.image.load(items[self.name]["image"]), (self.h-5, self.w-5))
+        self.item_rect = self.item_image.get_rect()
+        self.item_rect.x = self.rect.x + 2.5
+        self.item_rect.y = self.rect.y + 2.5
+
+        
+    def update(self):
+        self.count = self.font.render(str(self.count), True, WHITE)
+        if inventory.Active:
+            screen.blit(self.image, self.rect)
+            screen.blit(self.item_image, self.item_rect)
+            screen.blit(self.count_item, (self.slot_rect.x + 45, self.slot_rect.y + 45))
+slot = Slot(20, 20, "Apple", 0)
 
 running = True
 while running:
@@ -87,12 +140,23 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
             pygame.display.toggle_fullscreen()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_F1:
-            player.tool += 1
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F1:
+                player.tool += 1
+            if event.key == pygame.K_e:
+                if inventory.Active:
+                    inventory.Active = False
+                else:
+                    inventory.Active = True
     screen.fill(BLACK)
+    #Player setings
     player.update()
     player.moving()
     player.change_tools()
+    #Inventory setings
+    inventory.update()
+    slot.update()
+    
     pygame.display.flip()
 
     print(player.tool)
